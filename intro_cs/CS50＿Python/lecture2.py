5a6dc82c1c99c37a737a7d9a5a1c62004073bfc8
Leia Wang <leiwang@lab2r1s10.42abudhabi.ae>
2024-11-12 21:26:35 +0400

    update

--- intro_cs/CS50＿Python/lecture2.py
+++ intro_cs/CS50＿Python/lecture2.py
@@ -1,3 +1,6 @@
+#-*- coding: utf-8 -*-
+
+
 #conditions in python
 
 x = int(input("What's x? "))
@@ -37,3 +40,50 @@ elif x > y:
 else:
 	print("x equals y.")
 
+# here is another good example explaining how python take priority to conditions 
+score = int(input("Score is: "))
+
+if score >= 90:
+	print("A")
+elif score >= 80:
+	print("B")
+elif score >= 70:
+	print("C")
+elif score >= 60:
+	print("D")
+else:
+	print("F")
+
+
+#Functions in python
+	
+def is_odd(n):
+	if n % 2 == 1:
+		return True
+	else:
+		return False
+	
+# improved version:
+def is_odd(n):
+	return True if n % 2 ==1 else False
+
+# even impr0ved vrsion
+def is_odd(n):
+	return ( n % 2)
+	
+n = int(input("Is this number odd?"))
+if is_odd(n):
+	print("Yes it is an odd number")
+else:
+	print("No it is not odd number")
+
+# 判断是否是闰年
+
+year = int(input("Please enter a year number: "))
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+	print("闰年")
+else:
+	print("不是闰年")
+
+
+	
